cb8721bd5eeba7e7fd33570f5cb5e682cd0f460b
Deven Phillips <deven.phillips@gmail.com>
2017-02-20 11:27:24 -0500

    Replace forecast_weather/2 with a foreach/2

--- src/weather.erl
+++ src/weather.erl
@@ -6,7 +6,7 @@ forecast(CityList) ->
   AccumulatorPid = spawn(weather, accumulator, [self(), CityList, []]),
 
   % For each item in the list, launch a Process which will call the weather_api:get_weather/1 function
-  forecast_weather(AccumulatorPid, CityList),
+  lists:foreach(fun(City) -> spawn(weather, async_weather, [AccumulatorPid, City]) end, CityList),
   receive
     {done, CondList} ->
       order_results(CityList, CondList, [])
@@ -21,14 +21,6 @@ order_results([Head|Tail], Results, Accumulator) ->
   {City, Cond} = lists:keyfind(Head, 1, Results),
   io:format("Appending ~s for city ~s~n", [Cond, City]),
   order_results(Tail, Results, Accumulator ++ [Cond]).
-
-
-%% @doc Spawn a process to call async_weather/2 for each City in the CityList
-forecast_weather(_, []) ->
-  ok;
-forecast_weather(AccumulatorPid, [Head|Tail]) ->
-  spawn(weather, async_weather, [AccumulatorPid, Head]),
-  forecast_weather(AccumulatorPid, Tail).
 
 
 %% @doc Call weather_api:get_weather/1 for the specified City and send the results to AccumulatorPid
