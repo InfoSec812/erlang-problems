ed75f524352bea790f5ace4a4fbba48185550f9e
Deven Phillips <deven.phillips@gmail.com>
2017-02-20 10:51:39 -0500

    Implemented assured ordering

--- src/weather.erl
+++ src/weather.erl
@@ -1,21 +1,25 @@
 -module(weather).
 -export([forecast/1, accumulator/3, async_weather/2]).
 
-%% *********** WARNING ***********
-%% In a REAL application calling a web based API, there is no guarantee that the results will come back in the order
-%% in which they are sent... This can be addressed by using a Map instead of a List, or it is also possible to have a
-%% list of Tuples and iterate over them and use the source list to order the output.
-
 forecast(CityList) ->
   % Spawn an accumulator Process
   AccumulatorPid = spawn(weather, accumulator, [self(), CityList, []]),
 
   % For each item in the list, launch a Process which will call the weather_api:get_weather/1 function
-  forecast_weather(AccumulatorPid, CityList),
+  forecast_weather(AccumulatorPid, lists:reverse(CityList)),
   receive
     {done, CondList} ->
-      CondList
+      order_results(CityList, CondList, [])
   end.
+
+%% @doc Order the results in the same order as the input city list to ensure that differences in response times from the
+%% API calls won't cause the list to be out of order.
+order_results([], _, Accumulator) ->
+  Accumulator;
+order_results([Head|Tail], Results, Accumulator) ->
+  {City, Cond} = lists:keyfind(Head, 1, Results),
+  io:format("Appending ~s for city ~s~n", [Cond, City]),
+  order_results(Tail, Results, Accumulator ++ [Cond]).
 
 %% @doc Spawn a process to call async_weather/2 for each City in the CityList
 forecast_weather(_, []) ->
@@ -31,12 +35,10 @@ async_weather(AccumulatorPid, City) ->
 
 %% @doc Listen for messages from async_weather/2 and accumulate them until all specified Cities have been accounted for
 accumulator(ParentPid, [], State) ->
-  io:format("Sending results: ~p~n", [State]),
   ParentPid ! {done, State};
 accumulator(ParentPid, CityList, State) ->
   receive
     {City, {weather, {current, _, _}, {forecast, _, Cond}}} ->
       NewCityList = lists:delete(City, CityList),
-      io:format("City List: ~p~n", [NewCityList]),
-      accumulator(ParentPid, NewCityList, State ++ [Cond])
+      accumulator(ParentPid, NewCityList, State ++ [{City, Cond}])
   end.
