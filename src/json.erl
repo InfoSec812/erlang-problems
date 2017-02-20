1c22d2f13369f849ec10ba72338c9c70ef66b7fc
Deven Phillips <deven.phillips@gmail.com>
2017-02-20 10:31:01 -0500

    Implementation for Erlang problems

--- src/json.erl
+++ src/json.erl
@@ -1,4 +1,69 @@
 -module(json).
 -export([render/1]).
 
-render(_) -> ok.
+%% @doc Given a data structure consisting of (possibly nested) tuples/arrays/values, encode the data as a JSON string
+render({Key, Value}) ->
+  lists:flatten(io_lib:format("{~s}", [encode({Key, Value})]));
+render(Input) ->
+  lists:flatten(encode(Input)).
+
+-spec encode(Input :: term()) -> list().
+%% @doc Given a data structure consisting of (possibly nested) tuples/arrays/values, encode the data as an array of
+%% JSON strings
+%%
+%% @param The input term() which can be pretty much any Erlang type.
+%%
+%% @returns A <em>List</em> of Erlang terms which may contain strings, characters, and nested lists of the same
+%%
+%% @spec encode(Input::term()) -> list()
+encode(true) ->
+  "true";
+encode(false) ->
+  "false";
+encode(null) ->
+  "null";
+encode(undefined) ->
+  "null";
+encode(B) when is_binary(B) ->
+  encode_as_string(B);
+encode(I) when is_integer(I) ->
+  integer_to_list(I);
+encode(F) when is_float(F) ->
+  float_to_list(F);
+encode(A) when is_atom(A) ->
+  io_lib:format("\"~p\"", [A]);
+encode({Key, Value}) ->
+  io_lib:format("~s:~s", [encode(Key), encode(Value)]);
+encode({PropList}) when is_list(PropList) ->
+  [Head|Tail] = PropList,
+  io_lib:format("{~s}", [encode_as_array(Head, Tail)]);
+encode({PropList}) ->
+  io_lib:format("~s", [encode(PropList)]);
+encode(L) when is_list(L) ->
+  case misc_supp:is_string(L) of
+    true  -> encode_as_string(L);
+    false -> encode_as_array(L)
+  end;
+encode(Value) ->
+  io:format("~p", [Value]),
+  {error, "Invalid Data", Value}.
+
+encode_as_array([Head|Tail]) when is_tuple(Head), length(Head) == 2 ->
+  io_lib:format("~s", [encode_as_array(Head, Tail)]);
+encode_as_array([Head|Tail]) ->
+  io_lib:format("[~s]", [encode_as_array(Head, Tail)]).
+
+encode_as_array(Head, Tail) when length(Tail) == 0 ->
+  io_lib:format("~s", [encode(Head)]);
+encode_as_array(Head, Tail) ->
+  [NewHead|NewTail] = Tail,
+  io_lib:format("~s,~s", [encode(Head), encode_as_array(NewHead, NewTail)]).
+
+
+encode_as_string(B) ->
+  case is_binary(B) of
+    true  ->
+      lists:concat(["\"", binary_to_list(B), "\""]);
+    false ->
+      lists:concat(["\"", B, "\""])
+  end.
--- src/weather.erl
+++ src/weather.erl
@@ -1,4 +1,37 @@
 -module(weather).
--export([forecast/1]).
+-export([forecast/1, accumulator/3, async_weather/2]).
 
-forecast(_) -> ok.
+forecast(CityList) ->
+  % Spawn an accumulator Process
+  AccumulatorPid = spawn(weather, accumulator, [self(), CityList, []]),
+
+  % For each item in the list, launch a Process which will call the weather_api:get_weather/1 function
+  forecast_weather(AccumulatorPid, CityList),
+  receive
+    {done, CondList} ->
+      CondList
+  end.
+
+%% @doc Spawn a process to call async_weather/2 for each City in the CityList
+forecast_weather(_, []) ->
+  ok;
+forecast_weather(AccumulatorPid, [Head|Tail]) ->
+  spawn(weather, async_weather, [AccumulatorPid, Head]),
+  forecast_weather(AccumulatorPid, Tail).
+
+%% @doc Call weather_api:get_weather/1 for the specified City and send the results to AccumulatorPid
+async_weather(AccumulatorPid, City) ->
+  Weather = weather_api:get_weather(City),
+  AccumulatorPid ! {City, Weather}.
+
+%% @doc Listen for messages from async_weather/2 and accumulate them until all specified Cities have been accounted for
+accumulator(ParentPid, CityList, State) when length(CityList) == 0 ->
+  io:format("Sending results: ~p~n", [State]),
+  ParentPid ! {done, State};
+accumulator(ParentPid, CityList, State) ->
+  receive
+    {City, {weather, {current, _, _}, {forecast, _, Cond}}} ->
+      NewCityList = lists:delete(City, CityList),
+      io:format("City List: ~p~n", [NewCityList]),
+      accumulator(ParentPid, NewCityList, State ++ [Cond])
+  end.
